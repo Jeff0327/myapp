090ce5da6aac25173539e8908107fb43e3013af2
Jeff <cocacola158500@mgail.com>
2022-05-04 18:29:23 +0900

    0504

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,8 +1,13 @@
 import { authService } from "fbase";
+import { useNavigate } from "react-router-dom";
 import React from "react";
 
 const Profile = () => {
-  const onLogOutClick = () => authService.signOut();
+  const navigate = useNavigate();
+  const onLogOutClick = () => {
+    authService.signOut();
+    navigate("/");
+  };
   return (
     <>
       <button onClick={onLogOutClick}> Log out</button>
