0e54c0f25ef4c74f811e026349ef757f034e34d3
Jeff <cocacola158500@mgail.com>
2022-05-28 20:21:27 +0900

    0528_sec

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { brands } from "@fortawesome/fontawesome-svg-core/import.macro";
 const Navigation = ({ userObj }) => {
   return (
     <nav>
       <ul className="Navigation_lists">
-        <li>
+        <li className="Home_list">
           <Link to="/">Home</Link>
+          {/* <FontAwesomeIcon icon={brands} color={"#04AAFF"} size="2x" /> */}
         </li>
-        <li>
+        <li className="Profile_list">
           <Link to="/Profile">{userObj.displayName}'s Profile</Link>
         </li>
       </ul>
